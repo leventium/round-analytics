13d4f0025a20632d690233a975a3240fdee20bf9
Leon <limiroshnichenko@miem.hse.ru>
2022-12-04 12:43:29 +0300

    ⚰️ Remove unneeded code

--- round_analytics/functions.py
+++ round_analytics/functions.py
@@ -8,16 +8,3 @@ def check_env(*args: str):
                 f"No environment variable with name {name}. "
                 f"{args} must be specified."
             )
-
-
-def get_content_from_dir(path: str) -> dict[str, bytes]:
-    path = path.strip("/")
-    res = {}
-    filenames = [
-        f"{path}/{f}" for f in os.listdir(f"{path}/")
-        if os.path.isfile(f"{path}/{f}")
-    ]
-    for filename in filenames:
-        with open(filename, "r") as file:
-            res[filename.split("/")[1]] = file.read()
-    return res
